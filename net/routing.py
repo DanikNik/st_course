329ed7c3463c396341185f494a22a1d523903c27
d.nikolskiy <d.nikolskiy@corp.mail.ru>
2020-05-11 05:31:09 +0300

    routing table

--- net/routing.py
+++ net/routing.py
@@ -1,6 +1,15 @@
+from channel.connection import Connection
+
+
 class RoutingTable:
-    # a dict of "string" reprs of conn (addr) and a pyserial.Serial objects
+    # a dict of "string" reprs of conn (addr) and a Connection objects
     table = dict()
 
     def __getitem__(self, key):
         return self.table[key]
+
+    def add_entry(self, address, conn: Connection):
+        self.table[address] = conn
+
+    def remove_entry(self, address):
+        self.table.pop(address)
